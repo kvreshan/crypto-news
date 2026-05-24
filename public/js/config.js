4f0600fa066d7b7ec7736c4bd90481987dba1c26
kvreshan <150836086+kvreshan@users.noreply.github.com>
2026-05-25 00:12:44 +0530

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -3,8 +3,8 @@
 // =============================================
 
 const CONFIG = {
-  SUPABASE_URL: 'YOUR_SUPABASE_URL',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY',
+  SUPABASE_URL: 'https://hhpnejnbavtniwvadgfb.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhocG5lam5iYXZ0bml3dmFkZ2ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MjQ0ODUsImV4cCI6MjA5NTIwMDQ4NX0.i-I3BK5Z2aylYx_D8-rHDCKVENWLWSI-AXOVO69qtPA',
   SITE_NAME: 'CryptoSignals LK',
   TELEGRAM_URL: '',
   ARTICLES_PER_PAGE: 6,
